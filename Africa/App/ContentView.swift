d4461fed829f2f0687dc2fe4688107d240bb8109
newlandslucas <lucasnewlands.m@gmail.com>
2022-05-10 16:22:04 -0300

    feat: Cover Banner in MainView

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Home")
-            .padding()
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 280)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            .navigationTitle("Africa")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Lucas Newlands on 10/05/22.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
