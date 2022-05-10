f5b1dee90ce868d93e3553dc1d003728b3ddd06b
newlandslucas <lucasnewlands.m@gmail.com>
2022-05-10 15:51:37 -0300

    initial settings

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Lucas Newlands on 10/05/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("MapView")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Lucas Newlands on 10/05/22.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
